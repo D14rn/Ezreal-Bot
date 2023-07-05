e0f1432a9639f299d3ab3ff55e263e441f273542
D14rn <49399494+D14rn@users.noreply.github.com>
2023-07-05 22:17:34 +0200

    Add help cog to loaded extensions

--- src/ezreal.py
+++ src/ezreal.py
@@ -4,13 +4,17 @@ from discord.ext import commands
 
 class Ezreal(commands.Bot):
     def __init__(self, *args, intents: discord.Intents, guild_id: int=None):
-        super().__init__(args, intents=intents)
+        super().__init__(
+            args,
+            intents=intents,
+        )
         self.guild_id = guild_id
     
     async def setup_hook(self):
         await self.load_extension("cogs.general")
         await self.load_extension("cogs.math")
         await self.load_extension("cogs.ranked")
+        await self.load_extension("cogs.help")
 
         self.tree.copy_global_to(guild=self.guild_id)
         await self.tree.sync(guild=self.guild_id)
