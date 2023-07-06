cf1bcb80b6120a9719522fbaa3d0dc6f02a4bd8e
D14rn <49399494+D14rn@users.noreply.github.com>
2023-07-06 14:19:31 +0200

    Finish implementation of leagueV4 API endpoint

---
+++ src/api/leagueV4.py
@@ -0,0 +1,12 @@
+from riot import RiotAPI
+
+
+class MatchV5(RiotAPI):
+    _prefix = "/lol/league/v4/"
+    _endpoints = {
+        "by_summoner_id": "entries/by-summoner/#",
+    }
+
+    @classmethod
+    def by_summoner_id(cls, platform, summoner_id):
+        return cls._get_data(cls._get_url(platform, "by_summoner_id", summoner_id))
