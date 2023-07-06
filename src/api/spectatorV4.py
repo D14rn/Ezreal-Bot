bc6341c2e3abf794ed0c4298be7d3367caaf1cb7
D14rn <49399494+D14rn@users.noreply.github.com>
2023-07-06 14:36:31 +0200

    Finish spectatorV4 API endpoint implementation

---
+++ src/api/spectatorV4.py
@@ -0,0 +1,12 @@
+from riot import RiotAPI
+
+
+class MatchV5(RiotAPI):
+    _prefix = "/lol/spectator/v4/"
+    _endpoints = {
+        "by_summoner_id": "active-games/by-summoner/#",
+    }
+
+    @classmethod
+    def by_summoner_id(cls, region, summoner_id):
+        return cls._get_data(cls._get_url(region, "by_summoner_id", summoner_id))
